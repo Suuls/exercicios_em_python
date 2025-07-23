09ed4075c8304392f2768940bc39a2e23c761646
Saul <saul.evan2004@gmail.com>
2025-07-23 10:06:36 -0400

    feat: resultado exercicio 2

--- string.py
+++ string.py
@@ -1,6 +1,11 @@
-## A partir da string “!! ! a;b;c;d;e;f;gh!#########” gere o resultado
-## ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+# A partir da string “!! ! a;b;c;d;e;f;gh!#########” gere o resultado ['a', 'b', 'c', 'd', 'e', 'f', 'g']
 
 string = "!! ! a;b;c;d;e;f;gh!#########"
 print(string)
 print(string.strip(" !#h").split(";"))
+
+#A partir da string “ring ring! - hello!“ gere o resultado:
+
+string = "ring ring! - hello!"
+print(string)
+print(string.strip("ring ring!"))
